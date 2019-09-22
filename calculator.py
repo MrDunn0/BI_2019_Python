0fc6ad0287d4b78d4e0fbfd9a3b2a7c8b2c1389d
Mikhail <misha.grizzli@gmail.com>
2019-09-22 19:23:34 +0300

    Add input control and more clear interaction with user

--- calculator.py
+++ calculator.py
@@ -1,15 +1,34 @@
-input_data = []
-for i in range(3):
-    input_data.append(input())
+print("Привет! Я очень простой, но трудолюбивый калькулятор для 2-х целых чисел. Доступные операции: + - / *")
 
-if input_data[1] == "-":
-    print(int(input_data[0]) - int(input_data[2]))
+operators_list = ["+", "-", "/", "*"]
+while True:
+    first_num = input("Введите первое число: ")
+    operator = input("Введите оператор из предложенного списка: ")
+    second_num = input("Введите второе число: ")
+    if not (first_num.isdigit() and second_num.isdigit()):
+        print("Вместо чисел вы ввели что-то другое.\nПожалуйста, вводите только то, что указано, иначе я не пойму.")
+        continue
+    if (first_num == "0" or second_num == "0") and operator == "/":
+        print("Извините, Python не любит делить на ноль.")
+        continue
+    elif operator not in operators_list:
+        print("Вы ввели оператор, которого нет в списке.\nПожалуйста, вводите только то, что указано, иначе я не пойму.")
+        continue
+    break
 
-elif input_data[1] == "+":
-    print(int(input_data[0]) + int(input_data[2]))
+first_num, second_num = int(first_num), int(second_num)
+result = 0
 
-elif input_data[1] == "*":
-    print(int(input_data[0]) * int(input_data[2]))
+if operator == "+":
+    result = first_num + second_num
 
-elif input_data[1] == ":" or "/":
-    print(int(input_data[0]) / int(input_data[2]))
+elif operator == "-":
+    result = first_num - second_num
+
+elif operator == "/":
+    result = first_num / second_num
+
+elif operator == "*":
+    result = first_num * second_num
+
+print("Ответ: {} {} {} = {} \nCпасибо за обращение! Хорошего настроения!".format(first_num, operator, second_num, result))
