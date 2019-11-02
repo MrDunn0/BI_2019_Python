697b393a087ce90ce08767b343c21368064a3b3b
Mikhail <misha.grizzli@gmail.com>
2019-11-02 13:36:35 +0300

    Fix some PEP-problems

--- HW4_checkio/common_words.py
+++ HW4_checkio/common_words.py
@@ -11,6 +11,7 @@ def common_words(str1, str2):
             result.append(word)
     return ",".join(result)
 
-print(common_words("one,two,three", "four,five,one,two,six,three"))
-print(common_words("hello,world", "hello,earth"))
-print(common_words("one,two,three", "four,five,six"))
+
+# print(common_words("one,two,three", "four,five,one,two,six,three"))
+# print(common_words("hello,world", "hello,earth"))
+# print(common_words("one,two,three", "four,five,six"))
