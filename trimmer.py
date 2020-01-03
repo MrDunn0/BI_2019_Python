a7b482256b5bf9124239981e177510d84b14c887
Mikhail <misha.grizzli@gmail.com>
2020-01-03 03:50:17 +0300

    Fix PEP-8 problem with line length

--- trimmer.py
+++ trimmer.py
@@ -5,35 +5,36 @@ parser.add_argument(
     "fastq_file",
     type=str,
     help="Path to unzipped fastq-file."
-    )
+)
 parser.add_argument(
     "--min_length",
     type=int,
     default=0,
     help="Minimal length of reads to be selected. If not specified, then reads of any length will pass the filter."
-    )
+)
 parser.add_argument(
     "--keep_filtered",
     action="store_true",
     help="Reads that do not pass the filter will be written to a separate file."
-    )
+)
 parser.add_argument(
     "--gc_bounds",
     type=float,
     action="append",
     help="The range of quantitative content of GC. First and second values are min and max respectively."
          "If only one value is specified, then reads with greater or equal value will be selected."
-         "If not value specified, all reads will be selected.\n Example: \n python3 trimmer.py --gc_bounds 45 --gc_bounds 50 my_reads.fastq"
-    )
+         "If not value specified, all reads will be selected."
+         "Example: python3 trimmer.py --gc_bounds 45 --gc_bounds 50 my_reads.fastq"
+)
 parser.add_argument(
     "--output_base_name",
     type=str,
     help="Common prefix for output files"
-    )
+)
 args = parser.parse_args()
 
 if not args.output_base_name:
-    args.output_base_name = args.fastq_file[-6]
+    args.output_base_name = args.fastq_file[:-6]
 
 if args.gc_bounds:
     gc_min = args.gc_bounds[0]
@@ -49,7 +50,8 @@ def gc_content(read):
     return (read.count("G") + read.count("C")) / len(read) * 100
 
 
-with open(args.fastq_file, "r") as reads_input, open("{}_passed.fastq".format(args.output_base_name), 'a') as passed_output:
+with open(args.fastq_file, "r") as reads_input, open("{}_passed.fastq".format(args.output_base_name),
+                                                     'a') as passed_output:
     if args.keep_filtered:
         failed_output = open("{}_failed.fastq".format(args.output_base_name), "a")
 
